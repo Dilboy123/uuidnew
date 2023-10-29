92007ebe9c97b0090237a4cc86659cbc4af81b03
Dilanka Piyasekara <75825573+Dilboy123@users.noreply.github.com>
2023-10-29 07:44:05 +0530

    Add files via upload

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://20.81.134.47:8000/api/v1/";
+axios.defaults.baseURL = "https://tired-jade-lobster.cyclic.app/api/v1/";
 
 axios.interceptors.request.use(function (req) {
   const user = localStorage.getItem("user");
